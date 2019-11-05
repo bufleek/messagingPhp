dfe104c1e8986dc3e22a7ff45bee6a8a94d63a9a
bufleek <brunookiprop@gmail.com>
2019-11-05 10:03:41 +0300

    basic display of unread messages

--- adm/messages.php
+++ adm/messages.php
@@ -14,15 +14,39 @@ include "../classes/message.php";
     $messages = $messages->getMessages();
     foreach ($messages as $message) {
         //unread messages
-        if($message['status'] == 0){
-
+        if($message['m_status'] == 0){
+            echo'<div class="message">
+            <h4 class="sender">From :   '.$message['s_name'].'</h4>
+            <h5 class="email">'.$message['s_email'].'</h5>
+            <h4 class="subject">Subject ::'.$message['m_subject'].'</h4>
+            <a class="view"></a>
+            <a href="?mark_as_read&id=">mark as read</a>
+        </div>
+        <hr>';
         }
         //read messages
-        elseif ($message['status'] == 1) {
+        elseif ($message['m_status'] == 1) {
             
         }
     }
 ?>
+
+<style>
+    *{
+        padding:0;
+        margin:0;
+    }
+    .message{
+        background:#bbb;
+        padding:10px;
+    }
+    .message h5.email{
+        padding:0 0 0 30px;
+    }
+    .message h4.subject{
+        padding:10px 0 0 0;
+    }
+</style>
     
 </body>
 </html>
